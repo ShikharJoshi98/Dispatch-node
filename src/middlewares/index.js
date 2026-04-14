4c3a4a9e7e21183a12069d42b3c8258a41650ff7
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-04-14 11:24:05 +0530

    initializing the proj

--- src/middlewares/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    validateMiddleware: require('./validate.middleware')
-}
--- src/routes/v1/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const authRoutes = require('./auth.route');
-
-const router = express();
-
-router.use('/auth', authRoutes);
-
-module.exports = router;
