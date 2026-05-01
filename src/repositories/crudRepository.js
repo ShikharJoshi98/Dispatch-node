7f023fe7d130544adf22f912a8a8201de8d76a65
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-05-01 16:28:43 +0530

    crud repo setup

---
+++ src/repositories/crudRepository.js
@@ -0,0 +1,43 @@
+class CrudRepository{
+    constructor(model) {
+        this.model = model;
+    }
+
+    async create(data) {
+        const response = await this.model.create(data);
+        return response;
+    }
+
+    async fetchAll() {
+        const response = await this.model.findAll();
+        return response;
+    }
+
+    async fetchById(id) {
+        const response = await this.model.findByPk(id);
+        return response;
+    }
+
+    async update(id, data) {
+        const response = await this.model.update(
+            data,
+            {
+                where: {
+                    id: id
+                }
+            }
+        );
+        return response;
+    }
+
+    async destroy(id) {
+        const response = await this.model.destroy({
+            where: {
+                id: id
+            }
+        });
+        return response;
+    }
+}
+
+module.exports = CrudRepository;
